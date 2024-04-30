67ef061eab983b981dfeca41f1dec65752cf3e39
Alfie Le Feuvre <alfielefeuvre@icloud.com>
2024-04-30 20:58:18 +0100

    Initial Commit

---
+++ MaybeThisTime Watch App/MaybeThisTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaybeThisTimeApp.swift
+//  MaybeThisTime Watch App
+//
+//  Created by Alfie Le Feuvre on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MaybeThisTime_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
